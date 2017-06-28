ffec542cba1b8b22301413a0d5a68480f853a97a
Devin McGloin <devin@devinmcgloin.com>
2017-06-28 00:31:26 -0700

    Implemented RGB to HSV tests

--- clr/hex_test.go
+++ clr/hex_test.go
@@ -40,6 +40,34 @@ func TestHSL(t *testing.T) {
 	}
 }
 
+func TestHSV(t *testing.T) {
+	tables := []struct {
+		rgb RGB
+		h   uint16
+		s   uint8
+		v   uint8
+	}{
+		{RGB{R: 78, G: 91, B: 112}, 217, 30, 43},
+		{RGB{R: 165, G: 23, B: 139}, 310, 86, 64},
+		{RGB{R: 221, G: 132, B: 90}, 19, 59, 86},
+		{RGB{R: 89, G: 1, B: 55}, 323, 98, 34},
+		{RGB{R: 132, G: 135, B: 132}, 120, 2, 52},
+	}
+
+	for _, table := range tables {
+		h, s, v := table.rgb.HSV()
+		if h-table.h > 1 || table.h-h > 1 {
+			t.Errorf("H of %v was incorrect, got: %d wanted: %d\n", table.rgb, h, table.h)
+		}
+		if s != table.s {
+			t.Errorf("S of %v was incorrect, got: %d wanted: %d\n", table.rgb, s, table.s)
+		}
+		if v != table.v {
+			t.Errorf("V of %v was incorrect, got: %d wanted: %d\n", table.rgb, v, table.v)
+		}
+	}
+}
+
 func BenchmarkGenericColor(b *testing.B) {
 	colors := []RGB{
 		RGB{R: 78, G: 91, B: 112},
