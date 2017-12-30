6831ed552f1b9df8cabde42dfb2667ade6df7492
Devin McGloin <devin@devinmcgloin.com>
2017-12-29 18:02:58 -0800

    Simplified travis and abs

--- clr/hsv_test.go
+++ clr/hsv_test.go
@@ -19,10 +19,10 @@ func colorEquality(a RGB, b RGB) bool {
 	return abs(a.R, b.R) <= 1 && abs(a.G, b.G) <= 1 && abs(a.B, b.B) <= 1
 }
 
-func abs(a uint8, b uint8) uint8 {
+func abs(a uint8, b uint8) int {
 	c := int(a) - int(b)
 	if c < 0 {
 		c *= -1
 	}
-	return uint8(c)
+	return c
 }
